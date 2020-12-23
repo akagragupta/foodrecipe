d765b3791c5d515947229e1a839904fd4c473b2b
Akagra Gupta <17ucs012@lnmiit.ac.in>
2020-12-23 21:28:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
+import Recipe from './Recipe'
 import './App.css';
 
 function App() {
+
+  const APP_ID="fd512932";
+  const APP_KEY="47ff6a9434ccb3e0a4534ae90b7722d0";
+
+  const [recipes, setRecipes]= useState([]);
+  const [search, setSearch] =useState('');
+  const [query, setQuery] = useState("chicken");
+
+  // const exampleReq= `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  //useEffect is something that is going to run every time when you perform any action like click or something but adding an empty array runs 
+  // it once only its like a intialisation block and you can add any values too 
+
+  useEffect(()=>{
+    console.log("Effect has been run");
+    getReceipes();
+  }, [query])
+
+  const getReceipes= async ()=>{
+    const response= await fetch(`https:api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data= await response.json();
+    console.log(data);
+    setRecipes(data.hits);
+  }
+
+  const updateSearch = e =>{
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch= e=>{
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form" action="">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">search</button>
+      </form>
+      <div className="recipes">
+      {
+        recipes.map(recipe=>(
+          <Recipe key={recipe.recipe.label}
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients} />
+        ))
+      }
+      </div>
     </div>
   );
 }
